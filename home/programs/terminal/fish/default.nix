0e97a74b6b9f96897fd596545c7e8548e7bf1ea7
Jason Ridgway-Taylor <jasonr@intenda.uk.com>
2020-12-23 21:39:35 +0000

    Added fish packages

--- home/programs/terminal/fish/default.nix
+++ home/programs/terminal/fish/default.nix
@@ -5,16 +5,8 @@ let
     set -x FZF_DEFAULT_OPTS "--preview='bat {} --color=always'" \n
     set -x SKIM_DEFAULT_COMMAND "rg --files || fd || find ."
   '';
-	ponysay = pkgs.ponysay;
-	pokemonsay = mariusnur.pokemonsay;
-	cowsay = pkgs.callPackage <mariusnur/pkgs/cowsay.nix> {packages = [mariusnur.cowsay-files];};
-	lolcat = pkgs.lolcat;
         starship = pkgs.starship;
-	fortunefuturama = fetchzip {
-		url = "https://www.netmeister.org/apps/fortune-mod-futurama-0.2.tar.gz";
-		sha256 = "0b9zs0r2jml3cqiv5wcinhv1wmcgzk2r07wy298g08ya94x9xan6";
-	};
-	fortune = pkgs.fortune;
+
 
 in
 {
@@ -36,19 +28,7 @@ in
     '';
 
     interactiveShellInit = ''
-			set number (random 0 10)
-			if test $number -ge 8
-				${ponysay}/bin/ponysay --ponyonly --anypony
-			else if test $number -ge 5
-				${ponysay}/bin/ponysay -q
-			else if test $number -ge 3
-				${fortune}/bin/fortune | ${pokemonsay}/bin/pokemonsay
-			else if test $number -ge 1
-				${cowsay}/bin/cowsay -f (ls ${cowsay}/share/cows | shuf -n1) (${fortune}/bin/fortune)
-			else
-				${cowsay}/bin/cowsay -f zoidberg (${fortune}/bin/fortune ${fortunefuturama}/futurama)
- 			end
-                        source ("${starship}/bin/starship" init fish --print-full-init | psub)
+			 source ("${starship}/bin/starship" init fish --print-full-init | psub)
 		'';
 
     shellAliases = {
