2b81251c64d96ae943a2c75b17cd51e7819462a2
Jason Ridgway-Taylor <jasonr@intenda.uk.com>
2020-12-23 21:35:16 +0000

    Added fonts

--- home/programs/terminal/newsboat/default.nix
+++ home/programs/terminal/newsboat/default.nix
@@ -3,6 +3,10 @@
 {
     programs.newsboat = {
       enable = true;
+ urls = (map (x: {url = x; tags = ["maintaining"];}) [
+		"https://discourse.nixos.org/latest.rss"
+	  ]) ;
+		reloadTime = 5;
 
     };
 
