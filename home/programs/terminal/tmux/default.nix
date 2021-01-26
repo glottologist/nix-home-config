3d7fc71d408fdb26adffea854a9e05ac23d47ad7
Jason Ridgway-Taylor <jasonr@intenda.uk.com>
2021-01-26 15:59:54 +0000

    Adjusted tmux conf

--- home/programs/terminal/tmux/default.nix
+++ home/programs/terminal/tmux/default.nix
@@ -7,27 +7,27 @@ in
 {
   programs.tmux = {
     enable = true;
-    # aggressiveResize = true;
-    # baseIndex = 1;
-    extraConfig = "";
-    # escapeTime = 0;
-    # keyMode = "vi";
-    # plugins = with plugins; [
-    #   cpu
-    #   nord # theme
-    #   {
-    #     plugin = resurrect;
-    #     extraConfig = "set -g @resurrect-strategy-nvim 'session'";
-    #   }
-    #   {
-    #     plugin = continuum;
-    #     extraConfig = ''
-    #       set -g @continuum-restore 'on'
-    #       set -g @continuum-save-interval '60' # minutes
-    #     '';
-    #   }
-    # ];
-    # shortcut = "a";
-    # terminal = "screen-256color";
+    aggressiveResize = true;
+    baseIndex = 1;
+    extraConfig = tmuxConf;
+    escapeTime = 0;
+    keyMode = "vi";
+    plugins = with plugins; [
+      cpu
+      nord # theme
+      {
+        plugin = resurrect;
+        extraConfig = "set -g @resurrect-strategy-nvim 'session'";
+      }
+      {
+        plugin = continuum;
+        extraConfig = ''
+          set -g @continuum-restore 'on'
+          set -g @continuum-save-interval '60' # minutes
+        '';
+      }
+    ];
+    shortcut = "a";
+    terminal = "screen-256color";
   };
 }
