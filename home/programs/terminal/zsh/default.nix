8f6f90246375ee9a39f937adba31d819d84359b0
Jason Ridgway-Taylor <jasonr@intenda.uk.com>
2021-01-28 19:36:19 +0000

    removed shell init line

--- home/programs/terminal/zsh/default.nix
+++ home/programs/terminal/zsh/default.nix
@@ -7,6 +7,19 @@ let
   '';
   starship = pkgs.starship;
 
+  zshnixshell=  pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.1.0";
+          sha256 = "0snhch9hfy83d4amkyxx33izvkhbwmindy0zjjk28hih1a9l2jmx";
+        };
+
+    powerlevel10k   = pkgs.fetchFromGitHub {
+    owner = "romkatv";
+    repo = "powerlevel10k";
+    rev = "b7d90c84671183797bdec17035fc2d36b5d12292";
+    sha256 = "0nzvshv3g559mqrlf4906c9iw4jw8j83dxjax275b2wi8ix0wgmj";
+  };
 
 in
 {
@@ -16,23 +29,20 @@ in
     enable = true;
     plugins = [{
         name = "zsh-nix-shell";
-        file = "nix-shell.plugin.zsh";
-        src = pkgs.fetchFromGitHub {
-          owner = "chisui";
-          repo = "zsh-nix-shell";
-          rev = "v0.1.0";
-          sha256 = "0snhch9hfy83d4amkyxx33izvkhbwmindy0zjjk28hih1a9l2jmx";
-        };
- }];
+        src = zshnixshell;
+ }
+ 
+ {
+        name = "powerlevel10k";
+        src =powerlevel10k; 
+ }
+ ];
 
     promptInit = ''
       eval (direnv hook zsh)
       any-nix-shell zsh --info-right | source
     '';
 
-    interactiveShellInit = ''
-			 source ("${starship}/bin/starship" init zsh --print-full-init | psub)
-		'';
 
     shellAliases = {
       setmtu = "sudo ip link set dev eth0 mtu 1350"; 
